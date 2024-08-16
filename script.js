30411dde5fd08b89759ca1c2308456646b28baf2
Brian Engel <110498191+clutch42@users.noreply.github.com>
2024-08-16 11:10:20 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,13 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const titleBanner = document.querySelector('.title-banner');
+    const navbar = document.querySelector('.navbar');
+    const titleBannerHeight = titleBanner.offsetHeight;
+
+    window.addEventListener('scroll', function () {
+        if (window.scrollY > titleBannerHeight) {
+            navbar.classList.add('sticky');
+        } else {
+            navbar.classList.remove('sticky');
+        }
+    });
+});
